c89c1ebcb2a281c25785fb5864123a77c5970dc1
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-11-22 17:00:20 +0530

    Update - css modules in webpack config

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
+console.log('classes: ', classes);
+
+
+const layout = (props) => {
+    return (
+        <Aux>
+            <div> Toolbar, Sidebar, Backdrop </div>
+            <main className={classes.Content}> 
+                {props.children}
+            </main>
+        </Aux>
+    )
+}
+
+export default layout;
